c53e3790557cd07abac7f90a5a9914dc87dfd17c
xu <xu@sz1921680100>
2014-06-03 10:53:37 +0800

    颜色实体

---
+++ colorTest/src/com/color/test/ColorEntity.java
@@ -0,0 +1,27 @@
+package com.color.test;
+
+public class ColorEntity {
+
+	private String colorId;
+	private String colorCode;
+	private String colorName;
+	public String getColorId() {
+		return colorId;
+	}
+	public void setColorId(String colorId) {
+		this.colorId = colorId;
+	}
+	public String getColorCode() {
+		return colorCode;
+	}
+	public void setColorCode(String colorCode) {
+		this.colorCode = colorCode;
+	}
+	public String getColorName() {
+		return colorName;
+	}
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+	
+}
