29800bd28ebe4b9c60820eb9297dc41784bd015a
xuyouling425 <xue571@163.com>
2014-06-03 11:20:08 +0800

    first

---
+++ webTest/src/com/org/web/WebTest.java
@@ -0,0 +1,22 @@
+package com.org.web;
+
+public class WebTest {
+
+	private String webUrl;
+	private String webName;
+	public String getWebUrl() {
+		return webUrl;
+	}
+	public void setWebUrl(String webUrl) {
+		this.webUrl = webUrl;
+	}
+	public String getWebName() {
+		return webName;
+	}
+	public void setWebName(String webName) {
+		this.webName = webName;
+	}
+	
+	
+	
+}
